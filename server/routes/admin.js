bf239835c6e109f7f04f43f3491c7b7bb5ff4317
mahmoud Tarek <mahmoud_ta_rek@outlook.com>
2024-08-27 14:59:41 -0700

    Authorization miidleware Added

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -8,6 +8,21 @@ const jwtSecret = process.env.JWT_SECRET
 
 const adminLayout = "../views/layouts/admin"
 
+const authMiddleware = (req, res, next ) => {
+    const token = req.cookies.token;
+    if(!token) {
+      return res.status(401).json( { message: 'Unauthorized'} );
+    }
+
+    try {
+      const decoded = jwt.verify(token, jwtSecret);
+      req.userId = decoded.userId;
+      next();
+    } catch(error) {
+      res.status(401).json( { message: 'Unauthorized'} );
+    }
+  }
+
 //Sign In - Get Route
 router.get('/admin', async (req, res) => {
     const locals = {
