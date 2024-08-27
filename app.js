666255c5855267e91165039256469f55cc43956a
mahmoud Tarek <mahmoud_ta_rek@outlook.com>
2024-08-27 14:32:07 -0700

    connected database to express app

--- app.js
+++ app.js
@@ -6,3 +6,38 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const methodOverride = require('method-override');
+
+const connectDB = require('./server/config/db')
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+connectDB()
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cookieParser())
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) }
+  }));
+
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main'))
+app.use('/', require('./server/routes/admin'))
+
+
+app.listen(port, () => {
+    console.log(`app is listening on port ${port}`);
+});
